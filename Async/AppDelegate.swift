0d18d9d5f268db1ccf0fbacac651e4572b236415
regynald <raugustin95@gmail.com>
2015-09-26 14:36:09 -0400

    Fixed merge conflicts

--- Async/AppDelegate.swift
+++ Async/AppDelegate.swift
@@ -40,7 +40,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+//    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+//        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+//    }
 
 }
 
