bcc1b98dc58567f3fe2c2b58bafb86c840551263
regynald <raugustin95@gmail.com>
2015-09-26 02:15:18 -0400

    Basic storybording

---
+++ Async/asModeSelection.swift
@@ -0,0 +1,35 @@
+//
+//  asModeSelectionViewController.swift
+//  Async
+//
+//  Created by Regynald Augustin on 9/25/15.
+//  Copyright © 2015 HAM. All rights reserved.
+//
+
+import UIKit
+
+class asModeSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
